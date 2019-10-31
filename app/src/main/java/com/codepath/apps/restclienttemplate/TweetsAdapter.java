847772c4e47bd2f7a3b975fce6fd057bdd62c8ab
Cherie Young <44423254+youncher@users.noreply.github.com>
2019-10-31 05:12:56 -0700

    Tweets timelime is now displayed, and with relative time.

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,74 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder>{
+
+    Context context;
+    List<Tweet> tweets;
+
+    // Pass in context and list of tweets
+    public TweetsAdapter(Context context, List<Tweet> tweets) {
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+    // For each row, inflate the layout
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // Bind values based on position of the element
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Get the data at position
+        Tweet tweet = tweets.get(position);
+
+        // Bind the tweet with the (passed in) view holder
+        holder.bind(tweet);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tweets.size();
+    }
+
+    // Define a viewHolder
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivProfileImage;
+        TextView tvBody;
+        TextView tvScreenName;
+        TextView tvTime;
+
+        public ViewHolder(@NonNull View itemView) { // This itemView represents one row
+            super(itemView);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+            tvBody = itemView.findViewById(R.id.tvBody);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvTime = itemView.findViewById(R.id.tvTime);
+        }
+
+        public void bind(Tweet tweet) {
+            Glide.with(context).load(tweet.user.profileImageUrl).into(ivProfileImage);
+            tvBody.setText(tweet.body);
+            tvScreenName.setText(tweet.user.screenName);
+            tvTime.setText(TimeFormatter.getTimeDifference(tweet.createdAt));
+        }
+    }
+}
